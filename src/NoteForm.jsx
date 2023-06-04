cfdf3c5c32091598b46abc960f07bcbc7d71ee5c
Kevin Yang <e510021@gmail.com>
2023-06-05 00:33:19 +0800

    form select field

--- src/NoteForm.jsx
+++ src/NoteForm.jsx
@@ -1,26 +1,62 @@
+import { useRef } from "react";
+import { Link } from "react-router-dom";
+import CreatableSelect from "react-select/creatable";
+
 export default function NoteForm() {
+  const titleRef = useRef(null);
+  const markdownRef = useRef(null);
+
+  function submitForm(e) {
+    e.preventDefault();
+  }
+
   return (
-    <>
-      <form>
-        <div className="flex w-full gap-3">
-          <label htmlFor="title" className="flex flex-col gap-3 grow">
-            Title
-            <input
-              type="text"
-              className="border-2 border-slate-400 rounded-md px-2 py-1"
-              id="title"
-            />
-          </label>
-          <label htmlFor="tags" className="flex flex-col gap-3 grow">
-            Tags
-            <input
-              type="text"
-              className="border-2 border-slate-400 rounded-md px-2 py-1"
-              id="tags"
-            />
-          </label>
-        </div>
-      </form>
-    </>
+    <form onSubmit={submitForm}>
+      <div className="flex w-full gap-3">
+        <label htmlFor="title" className="flex flex-col gap-3 grow">
+          Title
+          <input
+            ref={titleRef}
+            type="text"
+            className="border border-slate-300 rounded-md px-2 py-1"
+            id="title"
+            required
+          />
+        </label>
+        <label htmlFor="tags" className="flex flex-col gap-3 grow">
+          Tags
+          <CreatableSelect isMulti required />
+        </label>
+      </div>
+      <div className="mt-5">
+        <label htmlFor="mark-down" className="block">
+          Body
+        </label>
+        <textarea
+          ref={markdownRef}
+          name=""
+          id="mark-down"
+          rows="15"
+          className="border border-slate-300 rounded-md px-1 py-1 w-full"
+          required
+        ></textarea>
+      </div>
+      <div className="flex justify-end gap-2 mt-3">
+        <button
+          type="submit"
+          className="bg-blue-500 text-white px-2 py-1 rounded-md hover:bg-blue-600"
+        >
+          Save
+        </button>
+        <Link to="..">
+          <button
+            type="button"
+            className="bg-red-500 text-white px-2 py-1 rounded-md hover:bg-red-600"
+          >
+            Cancel
+          </button>
+        </Link>
+      </div>
+    </form>
   );
 }
