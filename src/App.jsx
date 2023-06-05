5c6c7ef3bf15165dda0697a421211c4cd4e73480
Kevin Yang <e510021@gmail.com>
2023-06-05 23:52:53 +0800

    create note handler

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import NewNote from "./NewNote";
 import useLocalStorage from "./useLocalStorage";
 import { useMemo } from "react";
+import { v4 as uuidV4 } from "uuid";
 
 export default function App() {
   const [notes, setNotes] = useLocalStorage("notes", []);
@@ -15,11 +16,25 @@ export default function App() {
     });
   }, [notes, tags]);
 
+  function onCreateNote(noteData) {
+    setNotes((prevNotes) => {
+      return [
+        ...prevNotes,
+        {
+          title: noteData.title,
+          markdown: noteData.markdown,
+          id: uuidV4(),
+          tagIds: tags.map((tag) => tag.id),
+        },
+      ];
+    });
+  }
+
   return (
     <main className="container m-4">
       <Routes>
         <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/new" element={<NewNote />} />
+        <Route path="/new" element={<NewNote onSubmit={onCreateNote} />} />
         <Route path="/:id">
           <Route index element={<h1>Show</h1>} />
           <Route path="edit" element={<h1>edit</h1>} />
--- src/NewNote.jsx
+++ src/NewNote.jsx
@@ -1,10 +1,10 @@
 import NoteForm from "./NoteForm";
 
-export default function NewNote() {
+export default function NewNote({ onSubmit }) {
   return (
     <>
       <h1 className="mb-4">New Note</h1>
-      <NoteForm />
+      <NoteForm onSubmit={onSubmit} />
     </>
   );
 }
